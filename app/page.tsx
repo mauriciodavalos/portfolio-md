1a94d5cf06985eb2fc7aefe24a9f3a0264d70894
Mauricio Davalos <maudavalos1992@gmail.com>
2023-08-28 14:10:13 -0600

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,109 @@
-import NextLink from "next/link";
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code"
-import { button as buttonStyles } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import React from 'react';
+import { Link } from '@nextui-org/link';
+import { title, subtitle } from '@/components/primitives';
+import { Card, CardBody, CardHeader, CardFooter } from '@nextui-org/card';
+import { Divider } from '@nextui-org/divider';
+import { UilJavaScript, UilHtml5, UilReact } from '@iconscout/react-unicons';
+import {
+  BiLogoJavascript,
+  BiLogoAngular,
+  BiLogoMongodb,
+  BiLogoTailwindCss,
+  BiLogoTypescript,
+} from 'react-icons/bi';
+import { Image } from '@nextui-org/image';
 
 export default function Home() {
-	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Make&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-				<br />
-				<h1 className={title()}>
-					websites regardless of your design experience.
-				</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
-				</h2>
-			</div>
-
-			<div className="flex gap-3">
-				<Link
-					isExternal
-					as={NextLink}
-					href={siteConfig.links.docs}
-					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
-				>
-					Documentation
-				</Link>
-				<Link
-					isExternal
-					as={NextLink}
-					className={buttonStyles({ variant: "bordered", radius: "full" })}
-					href={siteConfig.links.github}
-				>
-					<GithubIcon size={20} />
-					GitHub
-				</Link>
-			</div>
-
-			<div className="mt-8">
-				<Snippet hideSymbol hideCopyButton variant="flat">
-					<span>
-						Get started by editing <Code color="primary">app/page.tsx</Code>
-					</span>
-				</Snippet>
-			</div>
-		</section>
-	);
+  return (
+    <div>
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block max-w-lg text-center justify-center">
+          <h1 className={title()}>Hello&nbsp;</h1>
+          <h1 className={title({ color: 'blue' })}>World!&nbsp;</h1>
+          <br />
+          <h1 className={title()}>- I´m Mauricio Davalos</h1>
+          <h2 className={subtitle({ class: 'mt-4' })}>
+            Full-Stack Developer & Web Designer
+          </h2>
+          <div className="flex justify-center">
+            <Image
+              isZoomed
+              width={200}
+              alt="NextUI Fruit Image with Zoom"
+              src="/FotoMau2.jpeg"
+              className="mt-3"
+            />
+          </div>
+        </div>
+      </section>
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block max-w-lg text-center justify-center">
+          <h1 className={title()}>About&nbsp;</h1>
+          <h1 className={title({ color: 'blue' })}>Me&nbsp;</h1>
+          <p className="text-justify m-1">
+            Hi there! My name is Mauricio Dávalos, but everyone calls me Mau or
+            Mauro. I'm passionate about technology and always seeking challenges
+            that allow me to develop my full potential. As a Full Stack
+            Developer, I have a deep understanding of the technologies powering
+            modern web applications.
+          </p>
+        </div>
+      </section>
+      <section>
+        <div className="flex justify-center">
+          <Card className="w-[70vw]">
+            <CardHeader className="flex gap-3">
+              <div className="flex flex-col">
+                <h2 className="text-center text-xl">My Skills</h2>
+              </div>
+            </CardHeader>
+            <Divider />
+            <div className="grid grid-cols-2">
+              <CardBody className="flex flex-row">
+                <BiLogoJavascript size="2em" color="#FFFF00" />
+                <p>JavaScript</p>
+              </CardBody>
+              <CardBody className="flex flex-row">
+                <UilHtml5 size="2em" color="#ff0000" />
+                <p>HTML</p>
+              </CardBody>
+              <CardBody className="flex flex-row">
+                <BiLogoTailwindCss size="2em" color="#0000ff" />
+                <p>CSS / Tailwind</p>
+              </CardBody>
+              <CardBody className="flex flex-row">
+                <UilReact size="2em" color="#61DAFB" />
+                <p>ReactJS</p>
+              </CardBody>
+              <CardBody className="flex flex-row">
+                <UilJavaScript size="2em" color="#008000" />
+                <p>NodeJS</p>
+              </CardBody>
+              <CardBody className="flex flex-row">
+                <BiLogoMongodb size="2em" color="#228b22" />
+                <p>MongoDB</p>
+              </CardBody>
+              <CardBody className="flex flex-row">
+                <BiLogoTypescript size="2em" color="#1e90ff" />
+                <p>TypeScript</p>
+              </CardBody>
+              <CardBody className="flex flex-row">
+                <BiLogoAngular size="2em" color="#ff0000" />
+                <p>Angular</p>
+              </CardBody>
+            </div>
+            <Divider />
+            <CardFooter>
+              <Link
+                isExternal
+                showAnchorIcon
+                href="https://github.com/nextui-org/nextui">
+                Visit source code on GitHub.
+              </Link>
+            </CardFooter>
+          </Card>
+        </div>
+      </section>
+    </div>
+  );
 }
